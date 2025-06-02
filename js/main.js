6d3d5df1f2b679147c5619e77733cccb5dcef98c
Nikita <powar810321@gmail.com>
2025-06-02 13:53:34 +0400

    testsend

--- js/main.js
+++ js/main.js
@@ -89,19 +89,19 @@ $(document).ready(function () {
    });
 
    //E-mail Ajax Send
-   $("form").submit(function () { //Change
-      var th = $(this);
-      $.ajax({
-         type: "POST",
-         url: "../mail.php", //Change
-         data: th.serialize()
-      }).done(function () {
-               $('.form__success').addClass('form__success-active');
-         setTimeout(function () {
-            // Done Functions
-            th.trigger("reset");
-         }, 1000);
-      });
-      return false;
-   });
+   // $("form").submit(function () { //Change
+   //    var th = $(this);
+   //    $.ajax({
+   //       type: "POST",
+   //       url: "../mail.php", //Change
+   //       data: th.serialize()
+   //    }).done(function () {
+   //             $('.form__success').addClass('form__success-active');
+   //       setTimeout(function () {
+   //          // Done Functions
+   //          th.trigger("reset");
+   //       }, 1000);
+   //    });
+   //    return false;
+   // });
 });
